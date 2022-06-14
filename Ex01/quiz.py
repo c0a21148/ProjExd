9395daa099919c7e5ee2ea376dbd666346d42005
shimoda <c0a21148fa@edu.teu.ac.jp>
2022-06-14 16:17:11 +0900

    クイズ#1

--- Ex01/quiz.py
+++ Ex01/quiz.py
@@ -3,8 +3,14 @@ quiz_katuo = ["カツオの妹の名前は？","ワカメ","わかめ"]
 quiz_tarao = ["タラオはカツオから見てどんな関係？","甥","おい","甥っ子","おいっこ",]
 print("問題："+quiz_sazae[0])
 sazae_ans = input("答えるんだ\n")
-for i in range (len(sazae_ans)):
+for i in range (len(sazae_ans)-1):
     if sazae_ans == quiz_sazae[i+1]:
+        print("正解")
+        break
+    elif sazae_ans == quiz_sazae[i+2]:
+        print("正解")
+        break
+    elif sazae_ans == quiz_sazae[i+3]:
         print("正解")
         break
     else:
@@ -12,8 +18,14 @@ for i in range (len(sazae_ans)):
 
 print("問題："+quiz_katuo[0])
 katuo_ans = input("答えるんだ\n")
-for i in range (len(katuo_ans)):
+for i in range (len(katuo_ans)-1):
     if katuo_ans == quiz_katuo[i+1]:
+        print("正解")
+        break
+    elif sazae_ans == quiz_sazae[i+2]:
+        print("正解")
+        break
+    elif sazae_ans == quiz_sazae[i+3]:
         print("正解")
         break
     else:
@@ -22,8 +34,14 @@ for i in range (len(katuo_ans)):
 
 print("問題："+quiz_tarao[0])
 tarao_ans = input("答えるんだ\n")
-for i in range (len(tarao_ans)):
+for i in range (len(tarao_ans)-1):
     if tarao_ans == quiz_tarao[i+1]:
+        print("正解")
+        break
+    elif sazae_ans == quiz_sazae[i+2]:
+        print("正解")
+        break
+    elif sazae_ans == quiz_sazae[i+3]:
         print("正解")
         break
     else:
