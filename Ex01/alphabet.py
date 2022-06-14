3622c9ace510a2c15006ea364e264147ce381fcd
shimoda <c0a21148fa@edu.teu.ac.jp>
2022-06-14 17:53:03 +0900

    アルファベット

---
+++ Ex01/alphabet.py
@@ -0,0 +1,14 @@
+import random
+global x,y
+alpha = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+x = []
+y = []
+
+print("対象文字")
+for i in range(10):
+    x.append(alpha[random.sample(alpha,25)])
+print(x)
+print("表示文字")
+for j in range(8):
+    y.append(x[random.randint(0,9)])
+print(y)
