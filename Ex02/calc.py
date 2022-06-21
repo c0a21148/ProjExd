6de2a3ffe4c04dd4c19f8f8675f5cdd1ec264320
shimoda <c0a21148fa@edu.teu.ac.jp>
2022-06-21 17:55:35 +0900

    課題２修正

--- Ex02/calc.py
+++ Ex02/calc.py
@@ -1,8 +1,9 @@
 import tkinter as tk
 import tkinter.messagebox as tkm
-x=0
-b=4
-r=1
+p=4
+b=1 
+d=0
+r=4
 c=0
 def button_click(event):
     btn = event.widget
@@ -15,6 +16,26 @@ def button_click(event):
         ans = eval(a)
         entry.delete(0,tk.END)
         entry.insert(tk.END,ans)
+
+    elif num =="%":
+        a = entry.get()
+        y = eval(a)/100
+        entry.delete(0,tk.END)
+        entry.insert(tk.END,y)
+
+    elif num =="**":
+        a = entry.get()
+        y = eval(a)/100
+        entry.delete(0,tk.END)
+        entry.insert(tk.END,y)
+
+    elif num =="!":
+        a = entry.get()
+        for i in range(a):
+            y *= eval(a)
+        entry.delete(0,tk.END)
+        entry.insert(tk.END,y)
+
     else:
         entry.insert(tk.END,f"{num}")
 
@@ -22,8 +43,8 @@ if __name__ == "__main__":
     root = tk.Tk()
     #root.geometry("300x600")
     root.title("計算機")
-    for i,num in enumerate([i for i in range(9,-1,-1)] + ["+"] + ["-"] + ["*"] + ["/"] +["="]):
-        button = tk.Button(root, text = f"{num}",bg="lavender" ,width=4, height=1,command=button_click,font=("Times New Roman", 30))
+    for i,num in enumerate([i for i in range(9,0,-1)] + ["+"] + ["-"] + ["*"] + ["/"] +["="]):
+        button = tk.Button(root, text = f"{num}",bg="lavender" ,width=3, height=1,command=button_click,font=("Times New Roman", 30))
         button.grid(row=r,column=c)
         button.bind("<1>",button_click)
         c+=1
@@ -31,7 +52,25 @@ if __name__ == "__main__":
             r += 1
             c = 0
 
+    for i,num in enumerate([i for i in ["%","**","!"]]): #deleteと()、割り算、％、√の表示
+        button = tk.Button(root, text = f"{num}", width = 3, height = 1, font=("Times New Roman", 30))
+        button.grid(row = b, column = d)
+        d += 1
+        button.bind("<1>", button_click)
+        if (i+1)%4 == 0:
+            b += 1
+            d = 0
+
+    for i,num in enumerate([i for i in ["+","-","*","/"]]):
+        if i == 4:
+            button = tk.Button(root, text = f"{num}", width = 3, height = 1,bg="blue", font=("Times New Roman", 30))
+        else:
+            button = tk.Button(root, text = f"{num}", width = 3, height = 1, font=("Times New Roman", 30))
+        button.grid(row = p, column = 3)
+        p += 1
+        button.bind("<1>", button_click)
+
 
     entry = tk.Entry(root,justify="right",width=10,font=("Times New Roman", 40))
-    entry.grid(row=0,columnspan=3)
+    entry.grid(row=0,columnspan=5)
     root.mainloop() 
