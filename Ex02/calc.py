94e1fab00c3cd925dbf67ab3739c7d3ff2dc4d29
shimoda <c0a21148fa@edu.teu.ac.jp>
2022-06-21 16:58:48 +0900

    演習課題２

--- Ex02/calc.py
+++ Ex02/calc.py
@@ -1,5 +1,7 @@
 import tkinter as tk
 import tkinter.messagebox as tkm
+x=0
+b=4
 r=1
 c=0
 def button_click(event):
@@ -18,16 +20,18 @@ def button_click(event):
 
 if __name__ == "__main__":
     root = tk.Tk()
-    #root.geometry("300x500")
-    root.title("電卓")
-    for i,num in enumerate([i for i in range(9,-1,-1)]+["+"]+["tax"]+["="]):
-        button = tk.Button(root, text=f"{num}", width=4, height=2,font=("Times New Roman", 30))
+    #root.geometry("300x600")
+    root.title("計算機")
+    for i,num in enumerate([i for i in range(9,-1,-1)] + ["+"] + ["-"] + ["*"] + ["/"] +["="]):
+        button = tk.Button(root, text = f"{num}",bg="lavender" ,width=4, height=1,command=button_click,font=("Times New Roman", 30))
         button.grid(row=r,column=c)
-        c+=1
         button.bind("<1>",button_click)
-        if (i+1)%3 == 0:
-            r+=1
-            c=0
+        c+=1
+        if (i + 1) % 3 == 0:
+            r += 1
+            c = 0
+
+
     entry = tk.Entry(root,justify="right",width=10,font=("Times New Roman", 40))
     entry.grid(row=0,columnspan=3)
     root.mainloop() 
