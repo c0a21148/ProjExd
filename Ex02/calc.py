9189b257acee2611d76ab1d64c70d950e9498139
shimoda <c0a21148fa@edu.teu.ac.jp>
2022-06-21 15:31:14 +0900

    問題６

--- Ex02/calc.py
+++ Ex02/calc.py
@@ -1,35 +1,25 @@
 import tkinter as tk
 import tkinter.messagebox as tkm
-
-root = tk.Tk()
-root.title("calc")
-root.geometry("300x600")
-r,t=1,0
-
+r=1
+c=0
 def button_click(event):
-    button=event.widget
-    txt = button["text"]
-    #tkm.showinfo("",f"{txt}が押されました。")
-    entry.insert(tk.END,txt)
+    btn = event.widget
+    num = btn["text"]
+    tkm.showinfo(num, f"[{num}]ボタンが押されました")
+    entry.insert(tk.END,f"{num}")
 
-for num in range(9,-1,-1):
-    button = tk.Button(root,
-                       
-                       text=f"{num}",
-                       command=button_click,
-                       font=("Times New Roman",30),
-                       width=4,
-                       height=2, 
-                       )
-
-    button.bind("<1>",button_click)
-    button.grid(row=r,column=t)
-    t += 1
-    if (num-1)%3 == 0:
-        r+=1
-        t=0
-
-entry = tk.Entry(root,justify="right",width=10,font=("Times New Roman",40))
-entry.grid(row=0,columnspan=3)
-
-root.mainloop()
+if __name__ == "__main__":
+    root = tk.Tk()
+    #root.geometry("300x500")
+    root.title("電卓")
+    for i,num in enumerate([i for i in range(9,-1,-1)]+["+"]+["="]):
+        button = tk.Button(root, text=f"{num}", width=4, height=2,font=("Times New Roman", 30))
+        button.grid(row=r,column=c)
+        c+=1
+        button.bind("<1>",button_click)
+        if (i+1)%3 == 0:
+            r+=1
+            c=0
+    entry = tk.Entry(root,justify="right",width=10,font=("Times New Roman", 40))
+    entry.grid(row=0,columnspan=3)
+    root.mainloop() 
