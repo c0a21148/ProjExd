33f626c961f3258c4fe6acb0a4c9e18fac87fe5f
shimoda <c0a21148fa@edu.teu.ac.jp>
2022-06-28 14:35:42 +0900

    練習6

---
+++ Ex03/maze.py
@@ -0,0 +1,18 @@
+import tkinter as tk
+
+def key_down(event):
+    global key
+    key = event.keysym
+    print(f"{key}キーがおされました")
+    
+
+root = tk.Tk()
+root.title("迷えるこうかとん")
+canvas = tk.Canvas(root,width=1500,height=900,bg="black")
+canvas.pack()
+tori = tk.PhotoImage(file="fig/9.png")
+cx,cy=300,400
+canvas.create_image(cx,cy,image=tori,tag="tori")
+key = ""
+root.bind("<KeyPress>",key_down)
+root.mainloop()
