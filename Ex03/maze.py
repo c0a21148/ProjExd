9c725aaf57a6eaa7bf83195b25264253d00bcd82
shimoda <c0a21148fa@edu.teu.ac.jp>
2022-06-28 17:28:51 +0900

    修正しました #15

--- Ex03/maze.py
+++ Ex03/maze.py
@@ -41,6 +41,7 @@ def main_proc():
 
     tori = tk.PhotoImage(file=f"fig/{c}.png")
     cx,cy = mx*100+50,my*100+50
+
     canvas.coords("tori",cx,cy)
     canvas.create_image(cx,cy,image=tori,tag="tori")
     root.after(100,main_proc)
@@ -51,13 +52,16 @@ if __name__ == "__main__":
     
     canvas = tk.Canvas(root,width=1500,height=900,bg="black")
     canvas.pack()
+
     maze_bg = maze_maker.make_maze(15,9)
     maze_maker.show_maze(canvas,maze_bg)
+
     c=2 #こうかとんの立ち絵を変更する変数
     tori = tk.PhotoImage(file=f"fig/{c}.png")
     mx,my=1,1
     cx,cy = mx*100+50,my*100+50
     canvas.create_image(cx,cy,image=tori,tag="tori")
+    
     key = ""
     root.bind("<KeyPress>",key_down)
     root.bind("<KeyRelease>",key_up)
