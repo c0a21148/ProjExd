e1737862fbf3f71f4f32390aeab759f95ed39dda
shimoda <c0a21148fa@edu.teu.ac.jp>
2022-06-28 15:46:58 +0900

    3限基本機能実装完了

--- Ex03/maze.py
+++ Ex03/maze.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import maze_maker
 
 def key_down(event):
     global key
@@ -11,14 +12,21 @@ def key_up(event):
     
 def main_proc():
     global cx,cy
-    delta = {
-        "":[0,0],
-        "Up":[0,-20],
-        "Down":[0,+20],
-        "Left":[-20,0],
-        "Right":[+20,0]
-    }
-    cx,cy = cx+delta[key][0], cy+delta[key][1]
+    global mx,my
+    #delta = {
+    #    "":[0,0],
+    #    "Up":[0,-20],
+    #    "Down":[0,+20],
+    #    "Left":[-20,0],
+    #    "Right":[+20,0]
+    #}
+    
+    if key == "Up" and maze_bg[my-1][mx] == 0: my -=1
+    if key == "Down" and maze_bg[my+1][mx] == 0: my +=1
+    if key == "Left" and maze_bg[my][mx-1] == 0: mx -=1
+    if key == "Right" and maze_bg[my][mx+1] == 0: mx +=1
+
+    cx,cy = mx*100+50,my*100+50
     canvas.coords("tori",cx,cy)
     root.after(50,main_proc)
 
@@ -27,11 +35,15 @@ if __name__ == "__main__":
     root.title("迷えるこうかとん")
     canvas = tk.Canvas(root,width=1500,height=900,bg="black")
     canvas.pack()
+    maze_bg = maze_maker.make_maze(15,9)
+    maze_maker.show_maze(canvas,maze_bg)
     tori = tk.PhotoImage(file="fig/9.png")
-    cx,cy=300,400
+    mx,my=1,1
+    cx,cy = mx*100+50,my*100+50
     canvas.create_image(cx,cy,image=tori,tag="tori")
     key = ""
     root.bind("<KeyPress>",key_down)
     root.bind("<KeyRelease>",key_up)
+    
     main_proc()
     root.mainloop()
