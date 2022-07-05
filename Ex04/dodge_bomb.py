d23fb85219512e31acde851df5adc4203fd604c0
shimoda <c0a21148fa@edu.teu.ac.jp>
2022-07-05 14:56:32 +0900

    練習６

--- Ex04/dodge_bomb.py
+++ Ex04/dodge_bomb.py
@@ -1,3 +1,4 @@
+import random 
 import pygame as pg
 import sys
 
@@ -14,6 +15,16 @@ def main():
     kkimg_rct = kkimg_sfc.get_rect()
     kkimg_rct.center = 900,400
 
+    bmimg_sfc = pg.Surface((20,20))
+    bmimg_sfc.set_colorkey((0,0,0))
+    pg.draw.circle(bmimg_sfc,(255,0,0),(10,10),10)
+    bmimg_rct = bmimg_sfc.get_rect()
+    bmimg_rct.centerx = random.randint(0,screen_rct.width)
+    bmimg_rct.centery = random.randint(0,screen_rct.height)
+    vx,vy = +1,+1
+
+
+
     while True:
         screen_sfc.blit(bgimg_sfc,bgimg_rct)
         screen_sfc.blit(kkimg_sfc,kkimg_rct)
@@ -25,7 +36,9 @@ def main():
         if key_states[pg.K_DOWN] == True: kkimg_rct.centery += 1
         if key_states[pg.K_LEFT] == True: kkimg_rct.centerx -= 1
         if key_states[pg.K_RIGHT] == True: kkimg_rct.centerx += 1
-        
+        screen_sfc.blit(kkimg_sfc,kkimg_rct)
+        bmimg_rct.move_ip(vx,vy)
+        screen_sfc.blit(bmimg_sfc,bmimg_rct)
         pg.display.update()
         clock.tick(1000)
 
